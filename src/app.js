b87ba1ea09c3f18f8e1d23eef73b025cdfb8dbfe
shafayeatsumit <shafayeatkabir@gmail.com>
2017-10-02 22:32:58 +0600

    adding header componenet inside provider

--- src/app.js
+++ src/app.js
@@ -3,13 +3,16 @@ import { View } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 //import { reducers } from './reducers'
+import { Header } from './components/common';
 
 const reducers = () => []
 //declare app componenet
 const App = () => {
 	return (
 		<Provider store={ createStore(reducers) }>
-		<View />
+			<View>
+				<Header headerText="Tech Stack"/>
+			</View>
 		</Provider>
 	)
 }
